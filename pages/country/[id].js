0363e47b71692c8d5b22aeb2d9aec0616cd15d65
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-06-04 18:14:03 +0200

    Update [id].js

--- pages/country/[id].js
+++ pages/country/[id].js
@@ -151,7 +151,7 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
   const covids = await fetch(
-    `https://disease.sh/v3/covid-19/countries/US%2C%20France%2C%20United%20Kingdom%2C%20China%2C%20India%2C%20Japan%2C%20Canada%2C%20Germany%2C%20Spain%2C%20Russia%2C%20South%20Korea%2C%20Egypt%2C%20Australia%2C%20Italy%2C%20Greece?yesterday=true&twoDaysAgo=true&allowNull=true`
+    `https://disease.sh/v3/covid-19/countries/US%2C%20France%2C%20United%20Kingdom%2C%20China%2C%20India%2C%20Japan%2C%20Canada%2C%20Germany%2C%20Spain%2C%20Russia%2C%20South%20Korea%2C%20Egypt%2C%20Australia%2C%20Italy%2C%20Greece%2C%20Israel%2C%20Palestine%2C%20Liechtenstein?yesterday=true&twoDaysAgo=true&allowNull=true`
   ).then((r) => r.json());
   return {
     paths: covids.map((covid) => ({
