f55629de1b528f72645f44dd54489e440a6be59d
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-06-15 16:48:51 +0200

    Update country.js

--- pages/country.js
+++ pages/country.js
@@ -60,7 +60,7 @@ export default function Home({ covids }) {
 
 export async function getStaticProps() {
   const covids = await fetch(
-    `https://disease.sh/v3/covid-19/countries/France%2CUS%2CUnited%20Kingdom%2CChina%2CIndia%2CJapan%2CCanada%2CGermany%2CSpain%2CRussia%2CSouth%20Korea%2CEgypt%2CAustralia%2CItaly%2CGreece%2CIsrael%2CPalestine%2CLiechtenstein%2CCameroon%2CSouth%20Africa%2CBrazil%2CIreland%2CNew%20Zealand%2CFrench%20Polynesia%2CMexico%2CSeychelles%2CAlgeria%2CTunisia%2CPortugal%2CDenmark%2CAfghanistan%2CIran%2CIceland%2CPhilippines%2CThailand%2CSwitzerland%2CTunisia%2CBotswana%2CMauritius%2CBelgium?yesterday=true&twoDaysAgo=true&allowNull=true`
+    `https://disease.sh/v3/covid-19/countries/France%2CUS%2CUnited%20Kingdom%2CChina%2CIndia%2CJapan%2CCanada%2CGermany%2CSpain%2CRussia%2CSouth%20Korea%2CEgypt%2CAustralia%2CItaly%2CGreece%2CIsrael%2CPalestine%2CLiechtenstein%2CCameroon%2CSouth%20Africa%2CBrazil%2CIreland%2CNew%20Zealand%2CFrench%20Polynesia%2CMexico%2CSeychelles%2CAlgeria%2CTunisia%2CPortugal%2CDenmark%2CAfghanistan%2CIran%2CIceland%2CPhilippines%2CThailand%2CSwitzerland%2CTunisia%2CBotswana%2CMauritius%2CBelgium%2CBhutan%2CKuwait%2CEcuador%2CNamibia%2CSenegal?yesterday=true&twoDaysAgo=true&allowNull=true`
   ).then((r) => r.json());
 
   return {
